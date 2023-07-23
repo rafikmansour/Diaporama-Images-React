9f5a467bea7dc236bb3b554ef9eb681dd2b0aa00
Rafik <90220428+rafikmansour@users.noreply.github.com>
2023-07-22 21:57:29 -0400

    slideshow.jsx

---
+++ slideshow.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+
+const Slideshow = ({ images }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  };
+
+  useEffect(() => {
+    // Auto-advance to the next slide every 3 seconds
+    const interval = setInterval(nextSlide, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="slideshow">
+      <button onClick={prevSlide}>&lt;</button>
+      <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} />
+      <button onClick={nextSlide}>&gt;</button>
+    </div>
+  );
+};
+
+// Exemple d'utilisation :
+const imagesList = [
+  'url_de_l_image_1.jpg',
+  'url_de_l_image_2.jpg',
+  'url_de_l_image_3.jpg',
+  // Ajoutez autant d'URL d'images que nécessaire
+];
+
+const App = () => {
+  return (
+    <div>
+      <h1>Diaporama d'Images</h1>
+      <Slideshow images={imagesList} />
+    </div>
+  );
+};
+
+export default App;
